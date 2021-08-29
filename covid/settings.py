3a2533217f7b6eca1f7b9256b5ce5263c4314165
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2021-08-29 13:53:04 +0300

    Procfile changes and db email config

--- covid/settings.py
+++ covid/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 
 
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_USE_SSL = config('EMAIL_USE_SSL')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tonnycaptain7@gmail.com'
+EMAIL_HOST_PASSWORD = 'ycjegnkxjujrlhmv'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
 
 # Static files (CSS, JavaScript, Images)
