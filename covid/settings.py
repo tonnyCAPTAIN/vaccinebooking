24d03d80e0b841b61906e4b64d67923349e634b6
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2021-09-01 09:13:24 +0300

    debug=false

--- covid/settings.py
+++ covid/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'x&j0kfs+q@yqqt2m@8g0kuo$se$ux=ny1q+_5$4nzsd0*#qel7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=False)
+# DEBUG = config('DEBUG', cast=bool, default=False)
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost','127.0.0.1','https://vaccine-book.herokuapp.com']
 
