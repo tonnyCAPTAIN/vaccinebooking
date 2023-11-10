ca46232c1ef27a87ae315d07407e4e4e23dc067f
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2023-11-10 14:47:20 +0300

    new

--- book_appointment/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Profile, Book, Doctor, Venue
-
-admin.site.register(Profile)
-admin.site.register(Book)
-admin.site.register(Doctor)
-admin.site.register(Venue)
